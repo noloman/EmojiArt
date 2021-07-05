ea1edd33bd05f0c76fa95dfbe66f99e4f026cf44
Manuel Lorenzo <manulorenzop@gmail.com>
2021-07-05 15:27:46 +0200

    add context menu

--- EmojiArt/View/PaletteChooser.swift
+++ EmojiArt/View/PaletteChooser.swift
@@ -29,6 +29,32 @@ struct PaletteChooser: View {
             Image(systemName: "paintpalette")
         }
         .font(emojiFont)
+        .contextMenu { contextMenu }
+    }
+    
+    @ViewBuilder
+    var contextMenu: some View {
+        AnimatedActionButton(title: "New", systemImage: "plus") {
+            store.insertPalette(named: "New", emojis: "", at: chosenPaletteIndex)
+        }
+        AnimatedActionButton(title: "Delete", systemImage: "minus.circle") {
+            chosenPaletteIndex = store.removePalette(at: chosenPaletteIndex)
+        }
+        goToMenu
+    }
+    
+    var goToMenu: some View {
+        Menu {
+            ForEach(store.palettes) { palette in
+                AnimatedActionButton(title: palette.name) {
+                    if let index = store.palettes.index(matching: palette) {
+                        chosenPaletteIndex = index
+                    }
+                }
+            }
+        } label: {
+            Label("Go To", systemImage: "text.insert")
+        }
     }
     
     var rollTransition: AnyTransition {
